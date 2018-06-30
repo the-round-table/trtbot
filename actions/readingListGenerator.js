9d9ef101cfadb3ee381fbf1cc996a00edb63f455
Benjamin Congdon <bencongdon96@gmail.com>
2018-06-30 07:27:14 -0700

    Refactor reading list

---
+++ actions/readingListGenerator.js
@@ -0,0 +1,58 @@
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const _ = require('lodash');
+const discord = require('discord.js');
+const moment = require('moment');
+const truncate = require('truncate');
+
+class ReadingListGenerator {
+  constructor(Submissions) {
+    this.Submissions = Submissions;
+  }
+
+  async generate(options = {}) {
+    const query = {
+      createdAt: {
+        [Op.gt]: moment()
+          .startOf('day')
+          .toDate()
+      }
+    };
+
+    if(options.guildId) {
+      query.guildId = options.guildId;
+    }
+
+    const records = (await this.Submissions.findAll({
+      where: query,
+      order: ['createdAt']
+    })).map(record => record.get({ plain: true }));
+      
+    const embed = new discord.RichEmbed()
+      .setTitle(`📚 Reading List for ${moment().format('MMMM D, YYYY')}`);
+
+    if(records.length === 0) {
+      embed.setDescription('Nothing posted today. 😭');
+      return embed;
+    } else {
+      embed.setDescription("Here's what was posted to the Discord today:");
+    }
+
+    _.chain(records)
+      .groupBy('channel')
+      .toPairs()
+      .sortBy(pair => pair[1].length)
+      .reverse()
+      .value()
+      .forEach(pair => {
+        embed.addField(
+          '#' + pair[0],
+          pair[1].map(sub => `- ${truncate(sub.title, 75)} (${sub.shortLink})`).join('\n')
+        );
+      });
+
+    return embed;
+  }
+}
+
+module.exports = ReadingListGenerator;
--- commands/submissions/readingList.js
+++ commands/submissions/readingList.js
@@ -1,10 +1,5 @@
 const commando = require('discord.js-commando');
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
-const _ = require('lodash');
-const discord = require('discord.js');
-const moment = require('moment');
-const truncate = require('truncate');
+const ReadingListGenerator = require('../../actions/readingListGenerator.js');
 
 module.exports = class ReadingListCommand extends commando.Command {
   constructor(client) {
@@ -17,43 +12,15 @@ module.exports = class ReadingListCommand extends commando.Command {
       guildOnly: false
     });
 
-    this.Submissions = client.Submissions;
+    this.readingListGenerator = new ReadingListGenerator(client.Submissions);
   }
 
   async run(msg, args) {
-    this.Submissions.findAll({
-      where: {
-        createdAt: {
-          [Op.gt]: moment()
-            .startOf('day')
-            .toDate()
-        }
-      },
-      order: ['createdAt']
-    })
-      .map(record => record.get({ plain: true }))
-      .then(records => {
-        if(records.length === 0) {
-          return message.reply('Nothing posted today. 😭');
-        }
+    const guildId = msg.guild ? msg.guild.id : undefined;
 
-        const embed = new discord.RichEmbed()
-          .setTitle(`📚 Reading List for ${moment().format('MMMM D, YYYY')}`)
-          .setDescription("Here's what was posted to the Discord today:");
+    msg.reply({
+      embed: await this.readingListGenerator.generate({guildId})
+    });
 
-        _.chain(records)
-          .groupBy('channel')
-          .toPairs()
-          .sortBy(pair => pair[1].length)
-          .reverse()
-          .value()
-          .forEach(pair => {
-            embed.addField(
-              '#' + pair[0],
-              pair[1].map(sub => `- ${truncate(sub.title, 75)} (${sub.shortLink})`).join('\n')
-            );
-          });
-        msg.reply({ embed });
-      });
   }
 };
