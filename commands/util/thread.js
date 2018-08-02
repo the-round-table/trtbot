6d9d4e405221e20319ab1c9ae693bf3e23051a52
Benjamin Congdon <bencongdon96@gmail.com>
2018-08-01 21:00:33 -0700

    Add thread-finder command

---
+++ commands/util/thread.js
@@ -0,0 +1,54 @@
+const commando = require('discord.js-commando');
+const util = require('../../config.js');
+const _ = require('lodash');
+
+module.exports = class ThreadCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'thread',
+      memberName: 'thread',
+      group: 'util',
+      description:
+        'Finds an overflow room and creates a discussion there with the tagged members',
+      examples: ['thread "some topic" @josh @ben'],
+      guildOnly: true,
+      args: [
+        {
+          key: 'topic',
+          prompt: 'The topic of the thread',
+          type: 'string'
+        },
+        {
+          key: 'users',
+          prompt: 'The users to include in the thread',
+          type: 'user',
+          infinite: true
+        }
+      ]
+    });
+  }
+
+  getThreadChannelGroup(guild) {
+    return guild.channels
+      .filter(channel => channel.type == 'category')
+      .find('name', util.THREAD_CHANNEL_GROUP);
+  }
+
+  getThreadChannel(guild) {
+    const categoryChannel = this.getThreadChannelGroup(guild);
+    if (categoryChannel) {
+      return _.sample(categoryChannel.children.array());
+    }
+  }
+
+  async run(msg, { topic, users }) {
+    const threadChannel = this.getThreadChannel(msg.guild);
+
+    if (threadChannel) {
+      users.push(msg.author)
+      threadChannel.send(`Here's a place to discuss "${topic}": ${users.join(', ')}\n👇 👇 👇`)
+    } else {
+      msg.reply("Unable to find a channel to put the thread in!")
+    }
+  }
+};
--- config.template.js
+++ config.template.js
@@ -3,5 +3,6 @@ module.exports = {
   YOUTUBE_API_KEY: '',
   BITLY_TOKEN: '',
   READING_LIST_CHANNEL: 'general',
-  ANNOUNCEMENTS_CHANNEL: 'general'
+  ANNOUNCEMENTS_CHANNEL: 'general',
+  THREAD_CHANNEL_GROUP: ''
 };
