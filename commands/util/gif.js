515ae8da2c3e2088777a60633bb4a6e025291019
Benjamin Congdon <bencongdon96@gmail.com>
2018-08-02 19:36:38 -0700

    Add gif command

---
+++ commands/util/gif.js
@@ -0,0 +1,38 @@
+const commando = require('discord.js-commando');
+const giphy = require('giphy-api')();
+const _ = require('lodash');
+
+const NO_QUERY = 'no_query';
+
+module.exports = class GifCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'gif',
+      memberName: 'gif',
+      group: 'util',
+      description:
+        'Finds a gif for you',
+      examples: ['gif', 'gif cats'],
+      guildOnly: false,
+      args: [
+        {
+          key: 'query',
+          prompt: 'What type of gif would you like?',
+          type: 'string',
+          default: NO_QUERY
+        }
+      ]
+    });
+  }
+
+  async run(msg, { query }) {
+    let gif;
+    if (query == NO_QUERY) {
+      gif = await giphy.random({ rating: 'pg' }).data;
+    } else {
+      gif = _.sample((await giphy.search(query)).data);
+      console.log(gif)
+    }
+    msg.reply(gif.embed_url);
+  }
+};
