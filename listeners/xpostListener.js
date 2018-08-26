c4361c985b8abc0c4e1cca70c1571def1d4e674a
Naren Dasan <naren@narendasan.com>
2018-08-26 15:44:11 -0700

    WIP: xposter

--- listeners/xpostListener.js
+++ listeners/xpostListener.js
@@ -6,11 +6,14 @@ module.exports = async message => {
     return;
   }
   const msg = message.content;
+  const guild = message.guild;
   const srcChannel = message.channel;
   const poster = message.submitter;  
 
-  destChannels = message.content.match(channelRegex);
-    
+  destChannelNames = message.content.match(channelRegex);
+  //Name has # preprended
+  destChannels = destChannelNames.map(d => guild.channels.find(d));
+  
   for (destChannel of destChannels) {
      destChannel.send(
        `x-posted from ${srcChannel}:by ${poster}:\n>"${msg}"`
