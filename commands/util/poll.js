32463b3bc18eccb616e1f2ba668517de2b6aa9cb
Ish Shah <ish@fastmail.com>
2018-09-20 17:12:19 -0700

    Create poll.js

---
+++ commands/util/poll.js
@@ -0,0 +1,44 @@
+const commando = require('discord.js-commando');
+const _ = require('lodash');
+
+const NO_QUERY = 'no_query';
+
+module.exports = class PollCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'poll',
+      memberName: 'poll',
+      group: 'util',
+      description: 'Creates polls',
+      examples: ['poll', 'poll opt0 opt1'],
+      guildOnly: false,
+      args: [
+        {
+          key: 'opts',
+          prompt: 'What poll options do you want?',
+          type: 'string',
+          default: NO_OPTS
+        }
+      ]
+    });
+  }
+
+  async run(msg, { opts }) {
+    let poll;
+    if (opts == NO_OPTS) {
+      poll_data = "No options provided";
+    } else {
+      poll_data = "React with one of the following emotes\n";
+      var opt_data = opts.split(" ");
+      for each (var option in opt_data) {
+        poll_data = poll_data + "emote: " + option + "\n";
+      }
+    }
+
+    if (!poll) {
+      msg.reply("Couldn't create poll.");
+    } else {
+      msg.reply(poll_data);
+    }
+  }
+};
