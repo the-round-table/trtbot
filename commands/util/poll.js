d2abf8220083e50884109944ebcaae7f564a26d1
Ish Shah <ish@ishshah.me>
2018-10-04 09:59:50 -0700

    travis nits

--- commands/util/poll.js
+++ commands/util/poll.js
@@ -19,26 +19,25 @@ module.exports = class PollCommand extends commando.Command {
           prompt: 'What poll options do you want?',
           type: 'string',
           infinite: true,
-          default: NO_OPTS
-        }
-      ]
+          default: NO_OPTS,
+        },
+      ],
     });
   }
 
   async run(msg, { opts }) {
-    let poll;
+    let poll_data;
     if (opts == NO_OPTS) {
-      poll_data = "No options provided";
+      poll_data = `No options provided`;
     } else {
-      poll_data = "React with one of the following emotes\n";
+      poll_data = `React with one of the following emotes\n`;
       var opt_data = opts.split(" "); 
       for (let emoji_ind = 0; emoji_ind < opt_data.length; emoji_ind++) {
-        poll_data = poll_data + `:${POLL_EMOTES[emoji_ind]}: ${option}\n`;
-        emoji_ind = emoji_ind + 1;
+        poll_data += `:${POLL_EMOTES[emoji_ind]}: ${option}\n`;
       }
     }
 
-    if (!poll) {
+    if (!poll_data) {
       msg.reply("Couldn't create poll.");
     } else {
       let resp_msg = msg.reply(poll_data);
