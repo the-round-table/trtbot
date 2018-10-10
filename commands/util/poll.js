eabf551c66a548504d0c6d7634a6b21465b2f484
Benjamin Congdon <bencongdon96@gmail.com>
2018-10-10 07:26:16 -0700

    Add question argument to poll command Fixes #80

--- commands/util/poll.js
+++ commands/util/poll.js
@@ -26,6 +26,11 @@ module.exports = class PollCommand extends commando.Command {
       guildOnly: false,
       args: [
         {
+          key: 'question',
+          prompt: 'What question should the poll ask?',
+          type: 'string',
+        },
+        {
           key: 'opts',
           prompt: 'What poll options do you want?',
           type: 'string',
@@ -36,14 +41,14 @@ module.exports = class PollCommand extends commando.Command {
     });
   }
 
-  async run(msg, { opts }) {
+  async run(msg, { question, opts }) {
     let poll_data;
-    if (opts == NO_OPTS) {
+    if (opts === NO_OPTS) {
       poll_data = `No options provided`;
     } else if (opts.length > POLL_EMOTES.length) {
       poll_data = `Too many options provided`;
     } else {
-      poll_data = `React with one of the following emotes:\n`;
+      poll_data = `**"${question}"**\n_React with one of the following emotes:_\n`;
       for (let emoji_ind = 0; emoji_ind < opts.length; emoji_ind++) {
         poll_data += `${POLL_EMOTES[emoji_ind]} ${opts[emoji_ind]}\n`;
       }
@@ -52,7 +57,7 @@ module.exports = class PollCommand extends commando.Command {
     if (!poll_data) {
       msg.reply("Couldn't create poll.");
     } else {
-      let resp_msg = await msg.reply(poll_data);
+      let resp_msg = await msg.channel.send(poll_data);
       for (let emoji_ind = 0; emoji_ind < opts.length; emoji_ind++) {
         await resp_msg.react(`${POLL_EMOTES[emoji_ind]}`);
       }
