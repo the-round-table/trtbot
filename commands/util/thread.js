b3441d64dbe7127e4a82354830dd0cd41d02c7ca
Benjamin Congdon <bencongdon96@gmail.com>
2018-10-11 07:51:48 -0700

    Finish adding links to threads Fixes #83

--- commands/util/thread.js
+++ commands/util/thread.js
@@ -1,8 +1,9 @@
 const commando = require('discord.js-commando');
 const discord = require('discord.js');
 const truncate = require('truncate');
-const util = require('../../config.js');
+const config = require('../../config.js');
 const _ = require('lodash');
+const { getMessageLink } = require('../../utils.js');
 
 module.exports = class ThreadCommand extends commando.Command {
   constructor(client) {
@@ -35,7 +36,7 @@ module.exports = class ThreadCommand extends commando.Command {
   getThreadChannelGroup(guild) {
     return guild.channels
       .filter(channel => channel.type == 'category')
-      .find('name', util.THREAD_CHANNEL_GROUP);
+      .find('name', config.THREAD_CHANNEL_GROUP);
   }
 
   getThreadChannel(guild) {
@@ -52,15 +53,24 @@ module.exports = class ThreadCommand extends commando.Command {
 
     if (threadChannel) {
       users.push(msg.author);
-      const embed = new discord.RichEmbed();
-      threadChannel.send(
-        `Here's a place to discuss "${topic}": ${users.join(', ')}.`, {
-          embed
+      const embed = new discord.RichEmbed({
+        description: `👉 [Thread Context](${getMessageLink(msg)})`,
+      });
+      const threadMsg = await threadChannel.send(
+        `Here's a place to discuss "${topic}": ${users.join(', ')}.`,
+        {
+          embed,
         }
       );
-      msg.reply(`You got it! I setup a thread in ${threadChannel}`);
+
+      const responseEmbed = new discord.RichEmbed({
+        description: `👉 [Created Thread](${getMessageLink(threadMsg)})`,
+      });
+      await msg.reply(`You got it! I setup a thread in ${threadChannel}`, {
+        embed: responseEmbed,
+      });
     } else {
-      msg.reply('Unable to find a channel to put the thread in!');
+      await msg.reply('Unable to find a channel to put the thread in!');
     }
   }
 };
